9166c48b29db91602bd46272e633eacf64d729e7
Jacob Baumel <jacob.a.baumel@gmail.com>
2025-07-26 23:04:53 -0500

    Updated main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
 
+#include "Pyros.h"
 #include "RCP.h"
 #include "Sensors.h"
+#include "Servos.h"
 
 void setup() {
+    // Comment/uncomment the two versions of setup code below to choose between pyro channels and sensors
+
+    // Setup code for sensor usage
     Serial.begin(115200);
     while(!Serial) {
         yield();
         delay(10);
     }
 
+    Peripherals::I2C0.begin();
+    Peripherals::I2C1.begin();
+    Peripherals::SPI0.begin();
+    Peripherals::SDSPI.begin();
+
     RCP::init();
     RCP::setReady(true);
     RCPDebug("[RCP] Initialization complete!");
+    Sensors::setup();
+    Pyros::setup();
+    Servos::setup();
+
+
+    // Setup code for pyro channel testing
+    // RCP::init();
+    // RCP::setReady(true);
+    // Pyros::setup();
+    // RCP::testNum = 0;
+    // RCP::testState = RCP_TEST_RUNNING;
+    // Test::tests[0]->initialize();
 }
 
 void loop() {
@@ -22,6 +44,7 @@ void loop() {
 }
 
 void RCP::systemReset() {
-    NVIC_SystemReset();
+    // NVIC_SystemReset();
+    RCPDebug("[RCP] Hardware resets dont work :(");
 }
 
